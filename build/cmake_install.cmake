cb9c44eb74fc60739056668afa81dd6cfd88bd78
Martin Cheung <martincheung@RLYLTPZ20.local>
2020-05-09 21:12:29 +0200

    Update build folder

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/martincheung/Desktop/Learning Resources/C++ Nanodegree/Udacity-Cpp-Nanodegree/Project3-Memory-Management-Chatbot
+# Install script for directory: /Users/martincheung/Desktop/Learning Resources/C++ Nanodegree/CppND-Memory-Management-Chatbot
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/martincheung/Desktop/Learning Resources/C++ Nanodegree/Udacity-Cpp-Nanodegree/Project3-Memory-Management-Chatbot/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/martincheung/Desktop/Learning Resources/C++ Nanodegree/CppND-Memory-Management-Chatbot/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
